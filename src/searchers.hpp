e348dbfd3a355030e1f6e2f31563f27f84efc644
moaz-karam <moaz.karam.ali@gmail.com>
2025-12-30 10:30:12 +0200

    reset the search if anything is selected

--- src/searchers.hpp
+++ src/searchers.hpp
@@ -149,6 +149,19 @@ protected:
                 cell.x <= maxX && cell.y <= maxY;
     }
 
+    virtual void resetSearch()
+    {
+        ArrayList<Vector2I> walls;
+        Hashtable<Vector2I, CellType>::HashIterator iter;
+        iter.begin(grid.table);
+        for (iter; iter.hasNext(); iter.next())
+        {
+            if (iter.getValue() == WALL) walls.push(iter.getKey());
+        }
+        clear();
+        for (int i = 0; i < walls.getSize(); i += 1) putToGrid(walls.get(i), WALL);
+    }
+
     virtual bool putToGrid(Vector2I key, CellType ct)
     {
         if (!isValidCell(key)) return false;
@@ -276,23 +289,12 @@ public:
     virtual void select(CellType ct)
     {
         selectedType = ct;
-        // if the source or the target are selected
-        // the search is restarted
-        if (selectedType == SOURCE || selectedType == TARGET)
-        {
-            ArrayList<Vector2I> walls;
-            Hashtable<Vector2I, CellType>::HashIterator iter;
-            iter.begin(grid.table);
-            for (iter; iter.hasNext(); iter.next())
-            {
-                if (iter.getValue() == WALL) walls.push(iter.getKey());
-            }
-            clear();
-            for (int i = 0; i < walls.getSize(); i += 1) putToGrid(walls.get(i), WALL);
-        }
+        // reset the search if some cell type is selected
+        resetSearch();
     }
     virtual void run()
     {
+        resetSearch();
         running = true;
 
         from.insert(sourcePos, Vector2I{.x = -1000, .y = -1000});
